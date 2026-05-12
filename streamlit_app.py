4aff3a11cd1ab839cefabb1ceaae201facd9a899
Karen Atis <klatiso@eafit.edu.co>
2026-05-12 12:46:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np 
+import numpy as np
 
 # 1. CONFIGURACIÓN VISUAL (CYBERPUNK / NEON)
 st.set_page_config(page_title="Estadísticas Ley 1780", layout="wide", initial_sidebar_state="expanded")
